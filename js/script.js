110f0f1b225bd2686349ef0e0616328a6f09614f
madyanes <madyanes.dev@gmail.com>
2024-10-13 12:35:11 +0700

    Create UI of search form

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 const navbarNav = document.querySelector('.navbar-nav')
 const hamburgerMenu = document.querySelector('#hamburger-menu')
+const searchForm = document.querySelector('.search-form')
+const searchInput = document.querySelector('#search-box')
+const searchButton = document.querySelector('#search-button')
 
-// Toggle `active` class
+// Toggle `active` class for hamburger menu
 hamburgerMenu.onclick = () => {
   navbarNav.classList.toggle('active')
 }
 
-// Remove `active` class when we click on all elements except `hamburgerMenu` and `navbarNav`
+// Toggle `active` class for search form
+searchButton.onclick = () => {
+  searchForm.classList.toggle('active')
+  searchInput.focus()
+}
+
+// Remove `active` class when we click on all elements except the elements mentioned in the if statement
 document.onclick = (e) => {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active')
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active')
   }
 }
