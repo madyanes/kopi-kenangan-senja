bddaa6db8068b4babe0131d060fe2c601d298ac4
madyanes <madyanes.dev@gmail.com>
2024-10-13 19:29:45 +0700

    BUGFIX: cursor pointer for navbar buttons

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ const hamburgerButton = document.querySelector('#hamburger-menu')
 const searchForm = document.querySelector('.search-form')
 const searchInput = document.querySelector('#search-box')
 const searchButton = document.querySelector('#search-button')
+const shoppingCart = document.querySelector('.shopping-cart')
+const shoppingCartButton = document.querySelector('#shopping-cart-button')
 
 toggleActiveClass(hamburgerButton, navbarNav)
 toggleActiveClass(searchButton, searchForm, () => {
   searchInput.focus()
 })
+toggleActiveClass(shoppingCartButton, shoppingCart)
 
 document.onclick = (e) => {
   removeActiveClass(e, hamburgerButton, navbarNav)
   removeActiveClass(e, searchButton, searchForm)
+  removeActiveClass(e, shoppingCartButton, shoppingCart)
 }
