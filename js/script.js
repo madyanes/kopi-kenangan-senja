b3d57f82e12dec0da8f5c77c1c6c349b319d1db3
madyanes <madyanes.dev@gmail.com>
2024-10-14 03:40:02 +0700

    Create modal for products

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const searchInput = document.querySelector('#search-box')
 const searchButton = document.querySelector('#search-button')
 const shoppingCart = document.querySelector('.shopping-cart')
 const shoppingCartButton = document.querySelector('#shopping-cart-button')
+const itemDetailModal = document.querySelector('#item-detail-modal')
+const itemDetailButtons = document.querySelectorAll('.item-detail-button')
+const itemDetailCloseButton = document.querySelector('.modal .close-icon')
 
 toggleActiveClass(hamburgerButton, navbarNav)
 toggleActiveClass(searchButton, searchForm, () => {
@@ -18,4 +21,18 @@ document.onclick = (e) => {
   removeActiveClass(e, hamburgerButton, navbarNav)
   removeActiveClass(e, searchButton, searchForm)
   removeActiveClass(e, shoppingCartButton, shoppingCart)
+}
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = () => {
+    itemDetailModal.style.display = 'flex'
+  }
+})
+
+itemDetailCloseButton.onclick = () => {
+  itemDetailModal.style.display = 'none'
+}
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) itemDetailModal.style.display = 'none'
 }
